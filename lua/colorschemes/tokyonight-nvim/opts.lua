3047721e86c555f6db514bde35aa895b16973f21
Yuto Tanaka <git@mimikun.dev>
2026-02-24 13:23:21 +0900

    chore: update tokyonight.nvim opts

--- lua/colorschemes/tokyonight-nvim/opts.lua
+++ lua/colorschemes/tokyonight-nvim/opts.lua
@@ -1,13 +1,14 @@
 ---@type tokyonight.Config
 local opts = {
   -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
-  style = "moon", 
+  style = "moon",
   -- The theme is used when the background is set to light
-  light_style = "day", 
+  light_style = "day",
   -- Enable this to disable setting the background color
-  transparent = false, 
+  --- NOTE: use xiyaowong/transparent.nvim
+  transparent = vim.g.transparent_enabled,
   -- Configure the colors used when opening a `:terminal` in Neovim
-  terminal_colors = true, 
+  terminal_colors = true,
   styles = {
     -- Style to be applied to different syntax groups
     -- Value is any valid attr-list value for `:help nvim_set_hl`
@@ -17,22 +18,22 @@ local opts = {
     variables = {},
     -- Background styles. Can be "dark", "transparent" or "normal"
     -- style for sidebars, see below
-    sidebars = "dark", 
+    sidebars = "dark",
     -- style for floating windows
-    floats = "dark", 
+    floats = "dark",
   },
 
   -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-  day_brightness = 0.3, 
+  day_brightness = 0.3,
 
   -- dims inactive windows
-  dim_inactive = false, 
+  dim_inactive = false,
 
   -- When `true`, section headers in the lualine theme will be bold
-  lualine_bold = false, 
+  lualine_bold = false,
 
   -- When set to true, the theme will be cached for better performance
-  cache = true, 
+  cache = true,
 
   ---@type table<string, boolean|{enabled:boolean}>
   plugins = {
