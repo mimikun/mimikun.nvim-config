1a157cafdc8bcdfbec62bb0256b26332c1137e7f
Yuto Tanaka <git@mimikun.dev>
2026-03-13 16:18:45 +0900

    feat: add TomDeneire/lazytree

---
+++ lua/plugins/lazytree/init.lua
@@ -0,0 +1,11 @@
+---@type LazySpec
+local spec = {
+  "TomDeneire/lazytree",
+  --lazy = false,
+  cmd = require("plugins.lazytree.cmds"),
+  opts = {},
+  --cond = false,
+  --enabled = false,
+}
+
+return spec
