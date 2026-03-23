1a97efae267e959917ab70f34704c4b7e740afcc
Yuto Tanaka <git@mimikun.dev>
2026-03-23 17:51:47 +0900

    feat: add alker0/chezmoi.vim

---
+++ lua/plugins/chezmoi-vim/init.lua
@@ -0,0 +1,16 @@
+---@type LazySpec
+local spec = {
+  "alker0/chezmoi.vim",
+  lazy = false,
+  init = function()
+    -- Regex pattern of path for ignoring file type detection
+    vim.g["chezmoi#detect_ignore_pattern"] = ""
+
+    -- Setting `false` makes this plugin create and use temporary buffer for making builtin filetype detection override wrong filetype
+    vim.g["chezmoi#use_tmp_buffer"] = true
+  end,
+  --cond = false,
+  --enabled = false,
+}
+
+return spec
