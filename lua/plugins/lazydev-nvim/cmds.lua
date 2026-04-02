7123bf7cf1e6cb22aee96d1f529a3c9aacb10cac
Yuto Tanaka <git@mimikun.dev>
2026-04-02 21:17:41 +0900

    feat: add folke/lazydev.nvim

---
+++ lua/plugins/lazydev-nvim/cmds.lua
@@ -0,0 +1,6 @@
+---@type table
+local cmds = {
+  "LazyDev",
+}
+
+return cmds
---
+++ lua/plugins/lazydev-nvim/dependencies.lua
@@ -0,0 +1,9 @@
+---@type LazySpec[]
+local dependencies = {
+  "DrKJeff16/wezterm-types",
+  "LelouchHe/xmake-luals-addon",
+  --"hrsh7th/nvim-cmp",
+  --"saghen/blink.cmp",
+}
+
+return dependencies
