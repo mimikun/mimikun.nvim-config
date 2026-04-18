65420676de1982d2a67431482e40d045c2ff70ff
Yuto Tanaka <git@mimikun.dev>
2026-04-18 13:35:54 +0900

    chore(img-clip-nvim): improved opts.lua

--- lua/plugins/img-clip-nvim/opts.lua
+++ lua/plugins/img-clip-nvim/opts.lua
@@ -8,44 +8,100 @@ local opts = {
       dir_path = vim.fn.expand("%:t:r")
       return dir_path
     end,
+
     ---@type string | fun(): string
-    extension = "png",
+    extension = function()
+      local extension = "png"
+      return extension
+    end,
+
     ---@type string | fun(): string
-    file_name = "%Y-%m-%d-%H-%M-%S",
+    file_name = function()
+      local file_name = "%Y-%m-%d-%H-%M-%S"
+      return file_name
+    end,
+
     ---@type boolean | fun(): boolean
-    use_absolute_path = false,
+    use_absolute_path = function()
+      local use_absolute_path = false
+      return use_absolute_path
+    end,
+
     ---@type boolean | fun(): boolean
-    relative_to_current_file = false,
+    relative_to_current_file = function()
+      local relative_to_current_file = false
+      return relative_to_current_file
+    end,
 
     -- logging options
     ---@type boolean | fun(): boolean
-    verbose = true,
+    verbose = function()
+      local verbose = true
+      return verbose
+    end,
 
     -- template options
     ---@type string | fun(context: table): string
-    template = "$FILE_PATH",
+    template = function()
+      local template = "$FILE_PATH"
+      return template
+    end,
+
     ---@type boolean | fun(): boolean
-    url_encode_path = false,
+    url_encode_path = function()
+      local url_encode_path = false
+      return url_encode_path
+    end,
+
     ---@type boolean | fun(): boolean
-    relative_template_path = true,
+    relative_template_path = function()
+      local relative_template_path = true
+      return relative_template_path
+    end,
+
     ---@type boolean | fun(): boolean
-    use_cursor_in_template = true,
+    use_cursor_in_template = function()
+      local use_cursor_in_template = true
+      return use_cursor_in_template
+    end,
+
     ---@type boolean | fun(): boolean
-    insert_mode_after_paste = true,
+    insert_mode_after_paste = function()
+      local insert_mode_after_paste = true
+      return insert_mode_after_paste
+    end,
+
     ---@type boolean | fun(): boolean
-    insert_template_after_cursor = true,
+    insert_template_after_cursor = function()
+      local insert_template_after_cursor = true
+      return insert_template_after_cursor
+    end,
 
     -- prompt options
     ---@type boolean | fun(): boolean
-    prompt_for_file_name = true,
+    prompt_for_file_name = function()
+      local prompt_for_file_name = true
+      return prompt_for_file_name
+    end,
+
     ---@type boolean | fun(): boolean
-    show_dir_path_in_prompt = false,
+    show_dir_path_in_prompt = function()
+      local show_dir_path_in_prompt = false
+      return show_dir_path_in_prompt
+    end,
 
     -- base64 options
     ---@type number | fun(): number
-    max_base64_size = 10,
+    max_base64_size = function()
+      local max_base64_size = 10
+      return max_base64_size
+    end,
+
     ---@type boolean | fun(): boolean
-    embed_image_as_base64 = false,
+    embed_image_as_base64 = function()
+      local embed_image_as_base64 = false
+      return embed_image_as_base64
+    end,
 
     -- image options
     ---@type string | fun(): string
@@ -63,18 +119,37 @@ local opts = {
     end,
 
     ---@type boolean | fun(): boolean
-    copy_images = false,
+    copy_images = function()
+      local copy_images = false
+      return copy_images
+    end,
+
     ---@type boolean | fun(): boolean
-    download_images = true,
+    download_images = function()
+      local download_images = true
+      return download_images
+    end,
+
     ---@type string[]
-    formats = { "jpeg", "jpg", "png" },
+    formats = {
+      "jpeg",
+      "jpg",
+      "png",
+    },
 
     -- drag and drop options
     drag_and_drop = {
       ---@type boolean | fun(): boolean
-      enabled = true,
+      enabled = function()
+        local enabled = true
+        return enabled
+      end,
+
       ---@type boolean | fun(): boolean
-      insert_mode = false,
+      insert_mode = function()
+        local insert_mode = false
+        return insert_mode
+      end,
     },
   },
 
@@ -82,39 +157,72 @@ local opts = {
   filetypes = {
     markdown = {
       ---@type boolean | fun(): boolean
-      url_encode_path = true,
+      url_encode_path = function()
+        local url_encode_path = true
+        return url_encode_path
+      end,
+
       ---@type string | fun(context: table): string
-      template = "![$CURSOR]($FILE_PATH)",
+      template = function()
+        local template = "![$CURSOR]($FILE_PATH)"
+        return template
+      end,
+
       ---@type boolean | fun(): boolean
-      download_images = false,
+      download_images = function()
+        local download_images = false
+        return download_images
+      end,
     },
 
     vimwiki = {
       ---@type boolean | fun(): boolean
-      url_encode_path = true,
+      url_encode_path = function()
+        local url_encode_path = true
+        return url_encode_path
+      end,
+
       ---@type string | fun(context: table): string
-      template = "![$CURSOR]($FILE_PATH)",
+      template = function()
+        local template = "![$CURSOR]($FILE_PATH)"
+        return templates
+      end,
+
       ---@type boolean | fun(): boolean
-      download_images = false,
+      download_images = function()
+        local download_images = false
+        return download_images
+      end,
     },
 
     html = {
       ---@type string | fun(context: table): string
-      template = '<img src="$FILE_PATH" alt="$CURSOR">',
+      template = function()
+        local template = '<img src="$FILE_PATH" alt="$CURSOR">'
+        return template
+      end,
     },
 
     tex = {
       ---@type boolean | fun(): boolean
-      relative_template_path = false,
+      relative_template_path = function()
+        local relative_template_path = false
+        return relative_template_path
+      end,
+
       ---@type string | fun(context: table): string
-      template = [[
+      template = function()
+        local template = [[
 \begin{figure}[h]
   \centering
   \includegraphics[width=0.8\textwidth]{$FILE_PATH}
   \caption{$CURSOR}
   \label{fig:$LABEL}
 \end{figure}
-    ]],
+    ]]
+
+        return template
+      end,
 
       ---@type table
       formats = {
@@ -125,42 +233,58 @@ local opts = {
       },
 
       ---@type boolean | fun(): boolean
-      use_absolute_path = true,
+      use_absolute_path = function()
+        local use_absolute_path = true
+        return use_absolute_path
+      end,
     },
 
     typst = {
       ---@type string | fun(context: table): string
-      template = [[
+      template = function()
+        local template = [[
 #figure(
   image("$FILE_PATH", width: 80%),
   caption: [$CURSOR],
 ) <fig-$LABEL>
-    ]],
+    ]]
+
+        return template
+      end,
     },
 
     rst = {
       ---@type string | fun(context: table): string
-      template = [[
+      template = function()
+        local template = [[
 .. image:: $FILE_PATH
    :alt: $CURSOR
    :width: 80%
-    ]],
+    ]]
+        return templates
+      end,
     },
 
     asciidoc = {
       ---@type string | fun(context: table): string
-      template = 'image::$FILE_PATH[width=80%, alt="$CURSOR"]',
+      template = function()
+        local tempalte = 'image::$FILE_PATH[width=80%, alt="$CURSOR"]'
+        return template
+      end,
     },
 
     org = {
       ---@type string | fun(context: table): string
-      template = [=[
+      template = function()
+        local template = [=[
 #+BEGIN_FIGURE
 [[file:$FILE_PATH]]
 #+CAPTION: $CURSOR
 #+NAME: fig:$LABEL
 #+END_FIGURE
-    ]=],
+    ]=]
+        return template
+      end,
     },
   },
 
