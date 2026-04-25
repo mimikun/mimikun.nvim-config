5f8a9957febfc07f1b029d23c2308412cc00a62e
Yuto Tanaka <git@mimikun.dev>
2026-04-25 14:04:54 +0900

    feat: add Rtarun3606k/TakaTime

---
+++ lua/plugins/takatime/init.lua
@@ -0,0 +1,16 @@
+---@type LazySpec
+local spec = {
+  "Rtarun3606k/TakaTime",
+  lazy = false,
+  cmd = require("plugins.takatime.cmds"),
+  event = require("plugins.takatime.events"),
+  opts = require("plugins.takatime.opts"),
+  --config = function()
+  --  --local opts = require("plugins.takatime.opts")
+  --  --require("taka-time").setup(opts)
+  --end,
+  --cond = false,
+  --enabled = false,
+}
+
+return spec
---
+++ lua/plugins/takatime/opts.lua
@@ -0,0 +1,9 @@
+---@type table
+local opts = {
+  --mongo_uri = "",
+  --debounce_seconds = 2,
+  debug = false,
+  --ignore_repos = {},
+}
+
+return opts
