ad4503285ca3f8ae5e2ad2edeeafe6be21c08468
Yuto Tanaka <git@mimikun.dev>
2026-04-26 11:51:43 +0900

    wip

---
+++ lua/plugins/lexima-vim/init.lua
@@ -0,0 +1,18 @@
+---@type LazySpec
+local spec = {
+  "cohama/lexima.vim",
+  --lazy = false,
+  --keys = require("plugins.lexima-vim.keys"),
+  --event = require("plugins.lexima-vim.events"),
+  --init = function()
+  --    INIT
+  --end,
+  --opts = require("plugins.lexima-vim.opts"),
+  --config = function()
+  --    INIT
+  --end,
+  --cond = false,
+  --enabled = false,
+}
+
+return spec
