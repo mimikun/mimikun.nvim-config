7fa07dc74b1c743363fc504d982456543431a20a
Yuto Tanaka <git@mimikun.dev>
2026-05-02 09:30:10 +0900

    wip

--- lua/denops-plugins/skkeleton/dependencies.lua
+++ lua/denops-plugins/skkeleton/dependencies.lua
@@ -1,15 +1,6 @@
 ---@type LazySpec[]
 local dependencies = {
-  -- TODO: it
-
-  --"neovim/nvim-lspconfig",
-  --"nvim-tree/nvim-web-devicons",
-  --"nvim-lua/plenary.nvim",
-  --"MunifTanjim/nui.nvim",
-  --"rcarriga/nvim-notify",
-  --"vim-denops/denops.vim",
-  --"nvim-telescope/telescope.nvim",
-  --{ "nvim-treesitter/nvim-treesitter", branch = "main" },
+  "vim-denops/denops.vim",
 }
 
 return dependencies
--- lua/denops-plugins/skkeleton/init.lua
+++ lua/denops-plugins/skkeleton/init.lua
@@ -2,20 +2,11 @@
 local spec = {
   "vim-skk/skkeleton",
   --lazy = false,
-  --dir = ""
-  --url = ""
-  --name = ""
-  --dev = false
-  --build = "",
-  --branch = "",
-  --tag = "",
-  --commit = "",
-  --version = "",
   --ft = require("denops-plugins.skkeleton.ft"),
   --cmd = require("denops-plugins.skkeleton.cmds"),
   --keys = require("denops-plugins.skkeleton.keys"),
   --event = require("denops-plugins.skkeleton.events"),
-  --dependencies = require("denops-plugins.skkeleton.dependencies"),
+  dependencies = require("denops-plugins.skkeleton.dependencies"),
   --init = function()
   --    INIT
   --end,
@@ -23,12 +14,6 @@ local spec = {
   --config = function()
   --    INIT
   --end,
-  --main = ""
-  --pin = false,
-  --submodules = false,
-  --module = false,
-  --priority = 1000,
-  --optional = false,
   cond = false,
   enabled = false,
 }
