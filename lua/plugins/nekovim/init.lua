441a64618cad76fbad449083991d49522f32afb8
Yuto Tanaka <git@mimikun.dev>
2026-05-03 16:40:02 +0900

    wip

--- lua/plugins/nekovim/init.lua
+++ lua/plugins/nekovim/init.lua
@@ -1,34 +1,26 @@
 ---@type LazySpec
 local spec = {
-  "mimikun/spec-template",
+  url = "https://git.sr.ht/~elisoli/nekovim",
   --lazy = false,
-  --dir = ""
-  --url = ""
-  --name = ""
-  --dev = false
+  --dir = "",
+  --name = "",
+  --dev = false,
   --build = "",
   --branch = "",
   --tag = "",
   --commit = "",
   --version = "",
   --ft = require("plugins.nekovim.ft"),
-  --ft = require("denops-plugins.nekovim.ft"),
   --cmd = require("plugins.nekovim.cmds"),
-  --cmd = require("denops-plugins.nekovim.cmds"),
   --keys = require("plugins.nekovim.keys"),
-  --keys = require("denops-plugins.nekovim.keys"),
   --event = require("plugins.nekovim.events"),
-  --event = require("denops-plugins.nekovim.events"),
   --dependencies = require("plugins.nekovim.dependencies"),
-  --dependencies = require("denops-plugins.nekovim.dependencies"),
   --init = function()
   --    INIT
   --end,
   --opts = require("plugins.nekovim.opts"),
-  --opts = require("denops-plugins.nekovim.opts"),
   --config = function()
   --local opts = require("plugins.nekovim.opts")
-  --local opts = require("denops-plugins.nekovim.opts")
   --end,
   --main = ""
   --pin = false,
