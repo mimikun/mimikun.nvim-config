dc24f78feed3129e13f363252f3a4f1a6de6c05b
Yuto Tanaka <git@mimikun.dev>
2026-05-03 21:14:50 +0900

    wip

--- lua/plugins/toggleterm-nvim/cmds.lua
+++ lua/plugins/toggleterm-nvim/cmds.lua
@@ -1,15 +1,14 @@
 ---@type table
 local cmds = {
-  "TermSelect", 
-  "TermExec", 
-  "TermNew", 
-  "ToggleTerm", 
-  "ToggleTermToggleAll", 
-  "ToggleTermSendVisualLines", 
-  "ToggleTermSendVisualSelection", 
-  "ToggleTermSendCurrentLine", 
-  "ToggleTermSetName", 
+  "TermSelect",
+  "TermExec",
+  "TermNew",
+  "ToggleTerm",
+  "ToggleTermToggleAll",
+  "ToggleTermSendVisualLines",
+  "ToggleTermSendVisualSelection",
+  "ToggleTermSendCurrentLine",
+  "ToggleTermSetName",
 }
 
 return cmds
-
