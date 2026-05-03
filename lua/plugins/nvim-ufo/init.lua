afc3bdb8c096083a469b86619742c0cede180476
Yuto Tanaka <git@mimikun.dev>
2026-05-03 18:36:06 +0900

    wip

---
+++ lua/plugins/nvim-ufo/init.lua
@@ -0,0 +1,43 @@
+---@type LazySpec
+local spec = {
+  "mimikun/spec-template",
+  --lazy = false,
+  --dir = ""
+  --url = ""
+  --name = ""
+  --dev = false
+  --build = "",
+  --branch = "",
+  --tag = "",
+  --commit = "",
+  --version = "",
+  --ft = require("plugins.nvim-ufo.ft"),
+  --ft = require("denops-plugins.nvim-ufo.ft"),
+  --cmd = require("plugins.nvim-ufo.cmds"),
+  --cmd = require("denops-plugins.nvim-ufo.cmds"),
+  --keys = require("plugins.nvim-ufo.keys"),
+  --keys = require("denops-plugins.nvim-ufo.keys"),
+  --event = require("plugins.nvim-ufo.events"),
+  --event = require("denops-plugins.nvim-ufo.events"),
+  --dependencies = require("plugins.nvim-ufo.dependencies"),
+  --dependencies = require("denops-plugins.nvim-ufo.dependencies"),
+  --init = function()
+  --    INIT
+  --end,
+  --opts = require("plugins.nvim-ufo.opts"),
+  --opts = require("denops-plugins.nvim-ufo.opts"),
+  --config = function()
+  --local opts = require("plugins.nvim-ufo.opts")
+  --local opts = require("denops-plugins.nvim-ufo.opts")
+  --end,
+  --main = ""
+  --pin = false,
+  --submodules = false,
+  --module = false,
+  --priority = 1000,
+  --optional = false,
+  cond = false,
+  enabled = false,
+}
+
+return spec
