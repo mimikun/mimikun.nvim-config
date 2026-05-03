a284d1d597e0c060a029968160e188b49aa9778f
Yuto Tanaka <git@mimikun.dev>
2026-05-03 21:13:00 +0900

    wip

--- lua/plugins/toggleterm-nvim/init.lua
+++ lua/plugins/toggleterm-nvim/init.lua
@@ -1,6 +1,6 @@
 ---@type LazySpec
 local spec = {
-  "mimikun/spec-template",
+        'akinsho/toggleterm.nvim', 
   --lazy = false,
   --dir = ""
   --url = ""
@@ -10,26 +10,20 @@ local spec = {
   --branch = "",
   --tag = "",
   --commit = "",
-  --version = "",
+        version = "*", 
   --ft = require("plugins.toggleterm-nvim.ft"),
-  --ft = require("denops-plugins.toggleterm-nvim.ft"),
   --cmd = require("plugins.toggleterm-nvim.cmds"),
-  --cmd = require("denops-plugins.toggleterm-nvim.cmds"),
   --keys = require("plugins.toggleterm-nvim.keys"),
-  --keys = require("denops-plugins.toggleterm-nvim.keys"),
   --event = require("plugins.toggleterm-nvim.events"),
-  --event = require("denops-plugins.toggleterm-nvim.events"),
   --dependencies = require("plugins.toggleterm-nvim.dependencies"),
-  --dependencies = require("denops-plugins.toggleterm-nvim.dependencies"),
   --init = function()
   --    INIT
   --end,
   --opts = require("plugins.toggleterm-nvim.opts"),
-  --opts = require("denops-plugins.toggleterm-nvim.opts"),
-  --config = function()
-  --local opts = require("plugins.toggleterm-nvim.opts")
-  --local opts = require("denops-plugins.toggleterm-nvim.opts")
-  --end,
+  config = function()
+  local opts = require("plugins.toggleterm-nvim.opts")
+  require("toggleterm").setup(opts)
+  end,
   --main = ""
   --pin = false,
   --submodules = false,
