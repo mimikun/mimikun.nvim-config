3f1f406fb8123a4869b31c15b821c85d8ff33498
Yuto Tanaka <git@mimikun.dev>
2026-05-04 14:06:43 +0900

    wip

---
+++ lua/plugins/template-nvim/CODES/lua/telescope/_extensions/find_template.lua
@@ -0,0 +1,77 @@
+local telescope = require('telescope')
+local finders = require('telescope.finders')
+local pickers = require('telescope.pickers')
+local make_entry = require('telescope.make_entry')
+local conf = require('telescope.config').values
+local actions = require('telescope.actions')
+local action_state = require('telescope.actions.state')
+
+local temp_list = function(opts)
+  local temp = require('template')
+  local list = temp.get_temp_list()
+  local curbuf = vim.api.nvim_get_current_buf()
+  if opts.filter_ft then
+    return list[vim.bo[curbuf].filetype] or {}
+  end
+
+  local res = {}
+  for _, v in pairs(list) do
+    res = vim.list_extend(res, v)
+  end
+  return res
+end
+
+local find_template = function(opts)
+  opts = opts or {}
+  if opts.name then
+    local dir = require('template').temp_dir
+    local path = vim.loop.os_uname().sysname == 'Windows_NT' and '\\' or '/'
+    local file = dir .. path .. opts.name
+    if vim.fn.filereadable(file) == 0 then
+      local ok, fd = pcall(vim.loop.fs_open, file, 'w', 420)
+      if not ok then
+        vim.notify("Couldn't create file " .. file)
+        return
+      end
+      vim.loop.fs_close(fd)
+    end
+  end
+
+  -- by default is set for type=insert
+  -- unless is explicitly set by the filter_ft option which takes precedence
+  local filter_ft = (opts.type == 'insert')
+  if opts.filter_ft ~= nil then
+    filter_ft = opts.filter_ft
+  end
+
+  local results = temp_list({ filter_ft = filter_ft })
+
+  local tbl = {
+    prompt_title = 'find in templates',
+    results_title = 'templates',
+    finder = finders.new_table({
+      results = results,
+      entry_maker = make_entry.gen_from_file(opts),
+    }),
+    previewer = conf.file_previewer(opts),
+    sorter = conf.file_sorter(opts),
+  }
+
+  if opts.type then
+    tbl.attach_mappings = function(prompt_bufnr, _)
+      actions.select_default:replace(function()
+        actions.close(prompt_bufnr)
+        local selection = action_state.get_selected_entry()
+        local tmp_name = vim.fn.fnamemodify(selection[1], ':t')
+        tmp_name = vim.split(tmp_name, '%.', { trimempty = true })[1]
+
+        vim.cmd('Template ' .. tmp_name)
+      end)
+      return true
+    end
+  end
+
+  pickers.new(opts, tbl):find()
+end
+
+return telescope.register_extension({ exports = { find_template = find_template } })
---
+++ lua/plugins/template-nvim/CODES/plugin/template.lua
@@ -0,0 +1,50 @@
+if vim.g.load_template then
+  return
+end
+
+vim.g.load_template = true
+
+local api = vim.api
+
+api.nvim_create_user_command('Template', function(args)
+  require('template'):generate_template(args.fargs)
+end, {
+  nargs = '+',
+  complete = function(arg, line)
+    local temp = require('template')
+    if not temp.temp_dir then
+      vim.notify('[template.nvim] please config the temp_dir variable')
+      return {}
+    end
+
+    local list = temp.get_temp_list()
+
+    local function match_item(ft)
+      return vim.tbl_map(function(s)
+        s = vim.fn.fnamemodify(s, ':t:r')
+        if arg and string.match(s, '^' .. arg) then
+          return s
+        end
+        return s
+      end, list[ft])
+    end
+
+    local ft = api.nvim_buf_get_option(0, 'filetype')
+    if list[ft] then
+      return match_item(ft)
+    end
+
+    local args = vim.split(line, '%s+', { trimempty = true })
+    if #args == 1 and not list[ft] then
+      return
+    end
+
+    if #args >= 2 and args[2]:find('%.%w+$') then
+      ft = vim.filetype.match({ filename = args[2] })
+    end
+
+    if ft then
+      return match_item(ft)
+    end
+  end,
+})
