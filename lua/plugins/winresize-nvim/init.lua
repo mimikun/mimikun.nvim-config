b5f854d98a1e917efbd9302a9f1407a9989e9adb
Yuto Tanaka <git@mimikun.dev>
2026-05-04 18:18:56 +0900

    feat: add pogyomo/winresize.nvim

---
+++ lua/plugins/winresize-nvim/init.lua
@@ -0,0 +1,10 @@
+---@type LazySpec
+local spec = {
+  "pogyomo/winresize.nvim",
+  --lazy = false,
+  event = require("plugins.winresize-nvim.events"),
+  --cond = false,
+  --enabled = false,
+}
+
+return spec
