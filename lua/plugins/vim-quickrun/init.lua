8d258f9b43df5eeb0d64f24285946e4f819a60d4
Yuto Tanaka <git@mimikun.dev>
2026-05-05 01:12:40 +0900

    wip

---
+++ lua/plugins/vim-quickrun/init.lua
@@ -0,0 +1,43 @@
+---@type LazySpec
+local spec = {
+  "mimikun/spec-template",
+  --lazy = false,
+  --dir = ""
+  --url = ""
+  --name = ""
+  --dev = false
+  --build = "",
+  --branch = "",
+  --tag = "",
+  --commit = "",
+  --version = "",
+  --ft = require("plugins.vim-quickrun.ft"),
+  --ft = require("denops-plugins.vim-quickrun.ft"),
+  --cmd = require("plugins.vim-quickrun.cmds"),
+  --cmd = require("denops-plugins.vim-quickrun.cmds"),
+  --keys = require("plugins.vim-quickrun.keys"),
+  --keys = require("denops-plugins.vim-quickrun.keys"),
+  --event = require("plugins.vim-quickrun.events"),
+  --event = require("denops-plugins.vim-quickrun.events"),
+  --dependencies = require("plugins.vim-quickrun.dependencies"),
+  --dependencies = require("denops-plugins.vim-quickrun.dependencies"),
+  --init = function()
+  --    INIT
+  --end,
+  --opts = require("plugins.vim-quickrun.opts"),
+  --opts = require("denops-plugins.vim-quickrun.opts"),
+  --config = function()
+  --local opts = require("plugins.vim-quickrun.opts")
+  --local opts = require("denops-plugins.vim-quickrun.opts")
+  --end,
+  --main = ""
+  --pin = false,
+  --submodules = false,
+  --module = false,
+  --priority = 1000,
+  --optional = false,
+  cond = false,
+  enabled = false,
+}
+
+return spec
