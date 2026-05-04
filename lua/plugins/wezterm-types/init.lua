66cf989507464267fd7b173f18a0c1412880d92d
Yuto Tanaka <git@mimikun.dev>
2026-05-04 13:05:28 +0900

    feat: add DrKJeff16/wezterm-types

---
+++ lua/plugins/wezterm-types/init.lua
@@ -0,0 +1,11 @@
+---@type LazySpec
+local spec = {
+  "DrKJeff16/wezterm-types",
+  --lazy = false,
+  --version = false,
+  event = require("plugins.wezterm-types.events"),
+  --cond = false,
+  --enabled = false,
+}
+
+return spec
