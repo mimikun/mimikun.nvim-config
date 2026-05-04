b085bf0a4e015101fbad260eaf9124f6f50199a3
Yuto Tanaka <git@mimikun.dev>
2026-05-04 17:08:12 +0900

    wip

---
+++ lua/plugins/wezterm-nvim/init.lua
@@ -0,0 +1,43 @@
+---@type LazySpec
+local spec = {
+  "mimikun/spec-template",
+  --lazy = false,
+  --dir = ""
+  --url = ""
+  --name = ""
+  --dev = false
+  --build = "",
+  --branch = "",
+  --tag = "",
+  --commit = "",
+  --version = "",
+  --ft = require("plugins.wezterm-nvim.ft"),
+  --ft = require("denops-plugins.wezterm-nvim.ft"),
+  --cmd = require("plugins.wezterm-nvim.cmds"),
+  --cmd = require("denops-plugins.wezterm-nvim.cmds"),
+  --keys = require("plugins.wezterm-nvim.keys"),
+  --keys = require("denops-plugins.wezterm-nvim.keys"),
+  --event = require("plugins.wezterm-nvim.events"),
+  --event = require("denops-plugins.wezterm-nvim.events"),
+  --dependencies = require("plugins.wezterm-nvim.dependencies"),
+  --dependencies = require("denops-plugins.wezterm-nvim.dependencies"),
+  --init = function()
+  --    INIT
+  --end,
+  --opts = require("plugins.wezterm-nvim.opts"),
+  --opts = require("denops-plugins.wezterm-nvim.opts"),
+  --config = function()
+  --local opts = require("plugins.wezterm-nvim.opts")
+  --local opts = require("denops-plugins.wezterm-nvim.opts")
+  --end,
+  --main = ""
+  --pin = false,
+  --submodules = false,
+  --module = false,
+  --priority = 1000,
+  --optional = false,
+  cond = false,
+  enabled = false,
+}
+
+return spec
