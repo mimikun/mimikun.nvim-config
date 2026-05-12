330d5b5392958fccb0de275efe74d8ced77ac461
Yuto Tanaka <git@mimikun.dev>
2026-05-12 18:13:27 +0900

    wip

--- lua/plugins/multiple-cursors-nvim/keys.lua
+++ lua/plugins/multiple-cursors-nvim/keys.lua
@@ -1,62 +1,106 @@
 ---@type LazyKeysSpec[]
 local keys = {
   {
-    -- TODO: it
-    "<lhs>",
-    function()
-      -- TODO: it
-    end,
-    mode = "n",
-    desc = "",
-    --expr = true,
-    --noremap = true,
+    "<C-j>",
+    --function()
+    "<Cmd>MultipleCursorsAddDown<CR>",
+    --end,
+    mode = { "n", "x" },
+    desc = "Add cursor and move down",
     silent = true,
   },
-}
-local _keys = {
-  { "<C-j>", "<Cmd>MultipleCursorsAddDown<CR>", mode = { "n", "x" }, desc = "Add cursor and move down" },
-  { "<C-k>", "<Cmd>MultipleCursorsAddUp<CR>", mode = { "n", "x" }, desc = "Add cursor and move up" },
-
-  { "<C-Up>", "<Cmd>MultipleCursorsAddUp<CR>", mode = { "n", "i", "x" }, desc = "Add cursor and move up" },
-  { "<C-Down>", "<Cmd>MultipleCursorsAddDown<CR>", mode = { "n", "i", "x" }, desc = "Add cursor and move down" },
+  {
+    "<C-k>",
+    --function()
+    "<Cmd>MultipleCursorsAddUp<CR>",
+    --end,
+    mode = { "n", "x" },
+    desc = "Add cursor and move up",
+    silent = true,
+  },
+  {
+    "<C-Up>",
+    --function()
+    "<Cmd>MultipleCursorsAddUp<CR>",
+    --end,
+    mode = { "n", "i", "x" },
+    desc = "Add cursor and move up",
+    silent = true,
+  },
+  {
+    "<C-Down>",
+    --function()
+    "<Cmd>MultipleCursorsAddDown<CR>",
+    --end,
+    mode = { "n", "i", "x" },
+    desc = "Add cursor and move down",
+    silent = true,
+  },
 
   {
     "<C-LeftMouse>",
+    --function()
     "<Cmd>MultipleCursorsMouseAddDelete<CR>",
+    --end,
     mode = { "n", "i" },
     desc = "Add or remove cursor on mouse click",
+    silent = true,
   },
   {
     "<C-Return>",
+    --function()
     "<Cmd>MultipleCursorsAddDelete<CR>",
+    --end,
     mode = { "n" },
     desc = "Add a locked cursor or remove an existing cursor",
+    silent = true,
   },
-
   {
     "<Leader>m",
+    --function()
     "<Cmd>MultipleCursorsAddVisualArea<CR>",
+    --end,
     mode = { "x" },
     desc = "Add cursors to the lines of the visual area",
+    silent = true,
   },
-
   { "<Leader>a", "<Cmd>MultipleCursorsAddMatches<CR>", mode = { "n", "x" }, desc = "Add cursors to cword" },
   {
     "<Leader>A",
+    --function()
     "<Cmd>MultipleCursorsAddMatchesV<CR>",
+    --end,
     mode = { "n", "x" },
     desc = "Add cursors to cword in previous area",
+    silent = true,
   },
-
   {
     "<Leader>d",
+    --function()
     "<Cmd>MultipleCursorsAddJumpNextMatch<CR>",
+    --end,
     mode = { "n", "x" },
     desc = "Add cursor and jump to next cword",
+    silent = true,
   },
-  { "<Leader>D", "<Cmd>MultipleCursorsJumpNextMatch<CR>", mode = { "n", "x" }, desc = "Jump to next cword" },
-
-  { "<Leader>l", "<Cmd>MultipleCursorsLock<CR>", mode = { "n", "x" }, desc = "Lock virtual cursors" },
+  {
+    "<Leader>D",
+    --function()
+    "<Cmd>MultipleCursorsJumpNextMatch<CR>",
+    --end,
+    mode = { "n", "x" },
+    desc = "Jump to next cword",
+    silent = true,
+  },
+  {
+    "<Leader>l",
+    --function()
+    "<Cmd>MultipleCursorsLock<CR>",
+    --end,
+    mode = { "n", "x" },
+    desc = "Lock virtual cursors",
+    silent = true,
+  },
 }
 
 return keys
