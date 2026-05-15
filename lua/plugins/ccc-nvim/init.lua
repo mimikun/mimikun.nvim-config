ac26426b847b0f2efcd481bb638e227c51d8b4ca
Yuto Tanaka <git@mimikun.dev>
2026-05-15 13:24:54 +0900

    wip

---
+++ lua/plugins/ccc-nvim/init.lua
@@ -0,0 +1,36 @@
+---@type LazySpec
+local spec = {
+  "mimikun/spec-template",
+  --lazy = false,
+  --url = "",
+  --name = "",
+  --dev = false,
+  --dir = "",
+  --build = "",
+  --branch = "",
+  --tag = "",
+  --version = "",
+  --commit = "",
+  --main = "",
+  --pin = false,
+  --submodules = false,
+  --module = false,
+  --optional = false,
+  --ft = require("plugins.ccc-nvim.ft"),
+  --cmd = require("plugins.ccc-nvim.cmds"),
+  --keys = require("plugins.ccc-nvim.keys"),
+  --event = require("plugins.ccc-nvim.events"),
+  --dependencies = require("plugins.ccc-nvim.dependencies"),
+  --init = function()
+  --  -- NOTE: INIT
+  --end,
+  --opts = require("plugins.ccc-nvim.opts"),
+  --config = function()
+  --  local opts = require("plugins.ccc-nvim.opts")
+  --end,
+  --priority = 1000,
+  cond = false,
+  enabled = false,
+}
+
+return spec
