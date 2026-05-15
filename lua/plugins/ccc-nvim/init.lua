7df8398166a750b0d025d81fe2537f22f6148e40
Yuto Tanaka <git@mimikun.dev>
2026-05-15 13:28:24 +0900

    wip

--- lua/plugins/ccc-nvim/init.lua
+++ lua/plugins/ccc-nvim/init.lua
@@ -1,36 +1,32 @@
 ---@type LazySpec
 local spec = {
-  "mimikun/spec-template",
+  "uga-rosa/ccc.nvim",
   --lazy = false,
-  --url = "",
-  --name = "",
-  --dev = false,
-  --dir = "",
-  --build = "",
-  --branch = "",
-  --tag = "",
-  --version = "",
-  --commit = "",
-  --main = "",
-  --pin = false,
-  --submodules = false,
-  --module = false,
-  --optional = false,
-  --ft = require("plugins.ccc-nvim.ft"),
   --cmd = require("plugins.ccc-nvim.cmds"),
   --keys = require("plugins.ccc-nvim.keys"),
   --event = require("plugins.ccc-nvim.events"),
   --dependencies = require("plugins.ccc-nvim.dependencies"),
-  --init = function()
-  --  -- NOTE: INIT
-  --end,
+  init = function()
+	-- Enable true color
+	vim.opt.termguicolors = true
+  end,
   --opts = require("plugins.ccc-nvim.opts"),
-  --config = function()
-  --  local opts = require("plugins.ccc-nvim.opts")
-  --end,
-  --priority = 1000,
-  cond = false,
-  enabled = false,
+  config = function()
+    local opts = require("plugins.ccc-nvim.opts")
+	local ccc = require("ccc")
+	local mapping = ccc.mapping
+
+	ccc.setup({
+	  -- Your preferred settings
+	  -- Example: enable highlighter
+	  highlighter = {
+	    auto_enable = true,
+	    lsp = true,
+	  },
+	})
+  end,
+  --cond = false,
+  --enabled = false,
 }
 
 return spec
