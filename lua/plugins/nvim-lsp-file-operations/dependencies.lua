987acbcbfbf9fdb672a8c13d0c018bcdb665b7e7
Yuto Tanaka <git@mimikun.dev>
2026-05-18 13:34:47 +0900

    feat: add antosha417/nvim-lsp-file-operations

---
+++ lua/plugins/nvim-lsp-file-operations/dependencies.lua
@@ -0,0 +1,9 @@
+---@type LazySpec[]
+local dependencies = {
+  "nvim-lua/plenary.nvim",
+  "nvim-tree/nvim-tree.lua",
+  "nvim-neo-tree/neo-tree.nvim",
+  "simonmclean/triptych.nvim",
+}
+
+return dependencies
---
+++ lua/plugins/nvim-lsp-file-operations/init.lua
@@ -0,0 +1,16 @@
+---@type LazySpec
+local spec = {
+  "antosha417/nvim-lsp-file-operations",
+  --lazy = false,
+  --event = require("plugins.nvim-lsp-file-operations.events"),
+  dependencies = require("plugins.nvim-lsp-file-operations.dependencies"),
+  --opts = require("plugins.nvim-lsp-file-operations.opts"),
+  config = function()
+    local opts = require("plugins.nvim-lsp-file-operations.opts")
+    require("lsp-file-operations").setup(opts)
+  end,
+  --cond = false,
+  --enabled = false,
+}
+
+return spec
