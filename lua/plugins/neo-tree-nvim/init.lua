9851ac0bcc53dad5c47f548572a8de98c91e372c
Yuto Tanaka <git@mimikun.dev>
2026-05-19 13:07:02 +0900

    wip

--- lua/plugins/neo-tree-nvim/init.lua
+++ lua/plugins/neo-tree-nvim/init.lua
@@ -4,10 +4,9 @@ local spec = {
   -- neo-tree will lazily load itself
   lazy = false,
   branch = "v3.x",
-  --ft = require("plugins.neo-tree-nvim.ft"),
   cmd = require("plugins.neo-tree-nvim.cmds"),
   --keys = require("plugins.neo-tree-nvim.keys"),
-  --event = require("plugins.neo-tree-nvim.events"),
+  event = require("plugins.neo-tree-nvim.events"),
   dependencies = require("plugins.neo-tree-nvim.dependencies"),
   --init = function()
   --  -- NOTE: INIT
