a9d3f35010252f394c9b0b8650a08e2818d778c1
Yuto Tanaka <git@mimikun.dev>
2026-06-29 13:37:16 +0900

    wip

--- lua/plugins/kulala-nvim/CODES/lazy.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "mistweaverco/kulala.nvim",
-  ft = { "http", "rest" },
-  -- Load before session save/restore so VimLeavePre and SessionLoadPost hooks are registered.
-  event = { "SessionLoadPost", "VimLeavePre" },
-  opts = {},
-}
