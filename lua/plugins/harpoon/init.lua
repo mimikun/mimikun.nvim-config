8dbe30caf1b3d68d8529b45102bd047ce4a473ee
Yuto Tanaka <git@mimikun.dev>
2026-06-30 15:48:07 +0900

    wip

--- lua/plugins/harpoon/init.lua
+++ lua/plugins/harpoon/init.lua
@@ -1,26 +1,13 @@
 ---@type LazySpec
 local spec = {
-  "mimikun/spec-template",
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
   --lazy = false,
-  --url = "",
-  --name = "",
-  --dev = false,
-  --dir = "",
-  --build = "",
-  --branch = "",
-  --tag = "",
-  --version = "",
-  --commit = "",
-  --main = "",
-  --pin = false,
-  --submodules = false,
-  --module = false,
-  --optional = false,
   --ft = require("plugins.harpoon.ft"),
   --cmd = require("plugins.harpoon.cmds"),
   --keys = require("plugins.harpoon.keys"),
   --event = require("plugins.harpoon.events"),
-  --dependencies = require("plugins.harpoon.dependencies"),
+  dependencies = require("plugins.harpoon.dependencies"),
   --init = function()
   --  -- NOTE: INIT
   --end,
@@ -28,7 +15,6 @@ local spec = {
   --config = function()
   --  local opts = require("plugins.harpoon.opts")
   --end,
-  --priority = 1000,
   cond = false,
   enabled = false,
 }
