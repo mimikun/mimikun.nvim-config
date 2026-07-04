8dfad25a0796c7bdebb8cb06471caf655d9d6396
Yuto Tanaka <git@mimikun.dev>
2026-07-04 12:44:39 +0900

    feat: add parwest/peeper-picker.nvim

---
+++ lua/plugins/peeper-picker-nvim/keys.lua
@@ -0,0 +1,35 @@
+---@type LazyKeysSpec[]
+local keys = {
+  {
+    "<leader>pp",
+    function()
+      require("peeper_picker").find()
+    end,
+    mode = {
+      "n",
+    },
+    {
+      desc = "Peeper Picker",
+      --expr = true,
+      --noremap = true,
+      silent = true,
+    },
+  },
+  {
+    "<leader>ph",
+    function()
+      require("peeper_picker").history()
+    end,
+    mode = {
+      "n",
+    },
+    {
+      desc = "Peeper Picker History",
+      --expr = true,
+      --noremap = true,
+      silent = true,
+    },
+  },
+}
+
+return keys
