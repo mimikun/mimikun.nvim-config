42babaebe5817634587133ed3e1ee159c04d6ece
Yuto Tanaka <git@mimikun.dev>
2026-07-18 23:11:15 +0900

    wip

---
+++ lua/plugins/nvim-metals/init.lua
@@ -0,0 +1,44 @@
+---@type LazySpec
+local spec = {
+  "scalameta/nvim-metals",
+  --lazy = false,
+  --url = "",
+  --name = "",
+  --dev = false,
+  --dir = "",
+  --build = "",
+  --branch = "",
+  --tag = "",
+  --version = "",
+  --commit = "",
+  --main = "",
+  --pin = false,
+  --submodules = false,
+  --module = false,
+  --optional = false,
+  --ft = require("plugins.nvim-metals.ft"),
+  --ft = require("denops-plugins.nvim-metals.ft"),
+  --cmd = require("plugins.nvim-metals.cmds"),
+  --cmd = require("denops-plugins.nvim-metals.cmds"),
+  --keys = require("plugins.nvim-metals.keys"),
+  --keys = require("denops-plugins.nvim-metals.keys"),
+  --event = require("plugins.nvim-metals.events"),
+  --event = require("denops-plugins.nvim-metals.events"),
+  --dependencies = require("plugins.nvim-metals.dependencies"),
+  --dependencies = require("denops-plugins.nvim-metals.dependencies"),
+  --init = function()
+  --  -- NOTE: INIT
+  --end,
+  --opts = require("plugins.nvim-metals.opts"),
+  --opts = require("denops-plugins.nvim-metals.opts"),
+  --config = function()
+  --  local opts = require("plugins.nvim-metals.opts")
+  --  local opts = require("denops-plugins.nvim-metals.opts")
+  --end,
+  --priority = 1000,
+  cond = false,
+  enabled = false,
+}
+
+return spec
+-- :%s/nvim-metals/
