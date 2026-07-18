06c982d9d8e530a2b15bcc975b6d694df701ab61
Yuto Tanaka <git@mimikun.dev>
2026-07-18 23:11:47 +0900

    wip

---
+++ lua/plugins/haskell-tools-nvim/init.lua
@@ -0,0 +1,44 @@
+---@type LazySpec
+local spec = {
+  "MrcJkb/haskell-tools.nvim",
+  --lazy = false,
+  --url = "",
+  --name = "",
+  --dev = false,
+  --dir = "",
+  --build = "",
+  --branch = "",
+  --tag = "",
+  --version = "",
+  --commit = "",
+  --main = "",
+  --pin = false,
+  --submodules = false,
+  --module = false,
+  --optional = false,
+  --ft = require("plugins.haskell-tools-nvim.ft"),
+  --ft = require("denops-plugins.haskell-tools-nvim.ft"),
+  --cmd = require("plugins.haskell-tools-nvim.cmds"),
+  --cmd = require("denops-plugins.haskell-tools-nvim.cmds"),
+  --keys = require("plugins.haskell-tools-nvim.keys"),
+  --keys = require("denops-plugins.haskell-tools-nvim.keys"),
+  --event = require("plugins.haskell-tools-nvim.events"),
+  --event = require("denops-plugins.haskell-tools-nvim.events"),
+  --dependencies = require("plugins.haskell-tools-nvim.dependencies"),
+  --dependencies = require("denops-plugins.haskell-tools-nvim.dependencies"),
+  --init = function()
+  --  -- NOTE: INIT
+  --end,
+  --opts = require("plugins.haskell-tools-nvim.opts"),
+  --opts = require("denops-plugins.haskell-tools-nvim.opts"),
+  --config = function()
+  --  local opts = require("plugins.haskell-tools-nvim.opts")
+  --  local opts = require("denops-plugins.haskell-tools-nvim.opts")
+  --end,
+  --priority = 1000,
+  cond = false,
+  enabled = false,
+}
+
+return spec
+-- :%s/haskell-tools-nvim/
