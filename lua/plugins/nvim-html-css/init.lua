93ed6dbb8fcdc90b1c69b683bd349ea614d22ff9
Yuto Tanaka <git@mimikun.dev>
2026-07-18 23:11:26 +0900

    wip

---
+++ lua/plugins/nvim-html-css/init.lua
@@ -0,0 +1,44 @@
+---@type LazySpec
+local spec = {
+  "Jezda1337/nvim-html-css",
+  --lazy = false,
+  --url = "",
+  --name = "",
+  --dev = false,
+  --dir = "",
+  --build = "",
+  --branch = "",
+  --tag = "",
+  --version = "",
+  --commit = "",
+  --main = "",
+  --pin = false,
+  --submodules = false,
+  --module = false,
+  --optional = false,
+  --ft = require("plugins.nvim-html-css.ft"),
+  --ft = require("denops-plugins.nvim-html-css.ft"),
+  --cmd = require("plugins.nvim-html-css.cmds"),
+  --cmd = require("denops-plugins.nvim-html-css.cmds"),
+  --keys = require("plugins.nvim-html-css.keys"),
+  --keys = require("denops-plugins.nvim-html-css.keys"),
+  --event = require("plugins.nvim-html-css.events"),
+  --event = require("denops-plugins.nvim-html-css.events"),
+  --dependencies = require("plugins.nvim-html-css.dependencies"),
+  --dependencies = require("denops-plugins.nvim-html-css.dependencies"),
+  --init = function()
+  --  -- NOTE: INIT
+  --end,
+  --opts = require("plugins.nvim-html-css.opts"),
+  --opts = require("denops-plugins.nvim-html-css.opts"),
+  --config = function()
+  --  local opts = require("plugins.nvim-html-css.opts")
+  --  local opts = require("denops-plugins.nvim-html-css.opts")
+  --end,
+  --priority = 1000,
+  cond = false,
+  enabled = false,
+}
+
+return spec
+-- :%s/nvim-html-css/
