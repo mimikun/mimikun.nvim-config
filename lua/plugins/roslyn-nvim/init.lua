7ee8cb3253f06642b217d3a6fc9684a51c913e0e
Yuto Tanaka <git@mimikun.dev>
2026-07-18 23:11:12 +0900

    wip

---
+++ lua/plugins/roslyn-nvim/init.lua
@@ -0,0 +1,44 @@
+---@type LazySpec
+local spec = {
+  "seblyng/roslyn.nvim",
+  --lazy = false,
+  --url = "",
+  --name = "",
+  --dev = false,
+  --dir = "",
+  --build = "",
+  --branch = "",
+  --tag = "",
+  --version = "",
+  --commit = "",
+  --main = "",
+  --pin = false,
+  --submodules = false,
+  --module = false,
+  --optional = false,
+  --ft = require("plugins.roslyn-nvim.ft"),
+  --ft = require("denops-plugins.roslyn-nvim.ft"),
+  --cmd = require("plugins.roslyn-nvim.cmds"),
+  --cmd = require("denops-plugins.roslyn-nvim.cmds"),
+  --keys = require("plugins.roslyn-nvim.keys"),
+  --keys = require("denops-plugins.roslyn-nvim.keys"),
+  --event = require("plugins.roslyn-nvim.events"),
+  --event = require("denops-plugins.roslyn-nvim.events"),
+  --dependencies = require("plugins.roslyn-nvim.dependencies"),
+  --dependencies = require("denops-plugins.roslyn-nvim.dependencies"),
+  --init = function()
+  --  -- NOTE: INIT
+  --end,
+  --opts = require("plugins.roslyn-nvim.opts"),
+  --opts = require("denops-plugins.roslyn-nvim.opts"),
+  --config = function()
+  --  local opts = require("plugins.roslyn-nvim.opts")
+  --  local opts = require("denops-plugins.roslyn-nvim.opts")
+  --end,
+  --priority = 1000,
+  cond = false,
+  enabled = false,
+}
+
+return spec
+-- :%s/roslyn-nvim/
