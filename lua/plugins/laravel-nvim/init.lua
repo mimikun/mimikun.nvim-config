d68dfb1b1055d74b70a51043d77021127f8b09e8
Yuto Tanaka <git@mimikun.dev>
2026-07-18 23:11:28 +0900

    wip

---
+++ lua/plugins/laravel-nvim/init.lua
@@ -0,0 +1,44 @@
+---@type LazySpec
+local spec = {
+  "adalessa/laravel.nvim",
+  --lazy = false,
+  --url = "",
+  --name = "",
+  --dev = false,
+  --dir = "",
+  --build = "",
+  --branch = "",
+  --tag = "",
+  --version = "",
+  --commit = "",
+  --main = "",
+  --pin = false,
+  --submodules = false,
+  --module = false,
+  --optional = false,
+  --ft = require("plugins.laravel-nvim.ft"),
+  --ft = require("denops-plugins.laravel-nvim.ft"),
+  --cmd = require("plugins.laravel-nvim.cmds"),
+  --cmd = require("denops-plugins.laravel-nvim.cmds"),
+  --keys = require("plugins.laravel-nvim.keys"),
+  --keys = require("denops-plugins.laravel-nvim.keys"),
+  --event = require("plugins.laravel-nvim.events"),
+  --event = require("denops-plugins.laravel-nvim.events"),
+  --dependencies = require("plugins.laravel-nvim.dependencies"),
+  --dependencies = require("denops-plugins.laravel-nvim.dependencies"),
+  --init = function()
+  --  -- NOTE: INIT
+  --end,
+  --opts = require("plugins.laravel-nvim.opts"),
+  --opts = require("denops-plugins.laravel-nvim.opts"),
+  --config = function()
+  --  local opts = require("plugins.laravel-nvim.opts")
+  --  local opts = require("denops-plugins.laravel-nvim.opts")
+  --end,
+  --priority = 1000,
+  cond = false,
+  enabled = false,
+}
+
+return spec
+-- :%s/laravel-nvim/
---
+++ lua/plugins/laravel-nvim/keys.lua
@@ -0,0 +1,23 @@
+---@type LazyKeysSpec[]
+local keys = {
+  {
+    -- TODO: it
+    "<lhs>",
+    function()
+      -- TODO: it
+    end,
+    mode = {
+      "n",
+      -- TODO: it
+      --"x",
+      --"v",
+    },
+    desc = "",
+    -- TODO: it
+    --expr = true,
+    --noremap = true,
+    silent = true,
+  },
+}
+
+return keys
