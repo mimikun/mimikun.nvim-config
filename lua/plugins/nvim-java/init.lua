ab06f1bd27c3cd5835d9f9e5382eaeb7afe5932e
Yuto Tanaka <git@mimikun.dev>
2026-07-18 23:11:39 +0900

    wip

---
+++ lua/plugins/nvim-java/init.lua
@@ -0,0 +1,44 @@
+---@type LazySpec
+local spec = {
+  "nvim-java/nvim-java",
+  --lazy = false,
+  --url = "",
+  --name = "",
+  --dev = false,
+  --dir = "",
+  --build = "",
+  --branch = "",
+  --tag = "",
+  --version = "",
+  --commit = "",
+  --main = "",
+  --pin = false,
+  --submodules = false,
+  --module = false,
+  --optional = false,
+  --ft = require("plugins.nvim-java.ft"),
+  --ft = require("denops-plugins.nvim-java.ft"),
+  --cmd = require("plugins.nvim-java.cmds"),
+  --cmd = require("denops-plugins.nvim-java.cmds"),
+  --keys = require("plugins.nvim-java.keys"),
+  --keys = require("denops-plugins.nvim-java.keys"),
+  --event = require("plugins.nvim-java.events"),
+  --event = require("denops-plugins.nvim-java.events"),
+  --dependencies = require("plugins.nvim-java.dependencies"),
+  --dependencies = require("denops-plugins.nvim-java.dependencies"),
+  --init = function()
+  --  -- NOTE: INIT
+  --end,
+  --opts = require("plugins.nvim-java.opts"),
+  --opts = require("denops-plugins.nvim-java.opts"),
+  --config = function()
+  --  local opts = require("plugins.nvim-java.opts")
+  --  local opts = require("denops-plugins.nvim-java.opts")
+  --end,
+  --priority = 1000,
+  cond = false,
+  enabled = false,
+}
+
+return spec
+-- :%s/nvim-java/
