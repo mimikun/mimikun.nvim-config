6fb73ef76ec6528f724a4542a1c038bc407247b8
Yuto Tanaka <git@mimikun.dev>
2026-07-18 23:11:42 +0900

    wip

---
+++ lua/plugins/nvim-jdtls/init.lua
@@ -0,0 +1,44 @@
+---@type LazySpec
+local spec = {
+  "mfussenegger/nvim-jdtls",
+  --lazy = false,
+  --url = "",
+  --name = "",
+  --dev = false,
+  --dir = "",
+  --build = "",
+  --branch = "",
+  --tag = "",
+  --version = "",
+  --commit = "",
+  --main = "",
+  --pin = false,
+  --submodules = false,
+  --module = false,
+  --optional = false,
+  --ft = require("plugins.nvim-jdtls.ft"),
+  --ft = require("denops-plugins.nvim-jdtls.ft"),
+  --cmd = require("plugins.nvim-jdtls.cmds"),
+  --cmd = require("denops-plugins.nvim-jdtls.cmds"),
+  --keys = require("plugins.nvim-jdtls.keys"),
+  --keys = require("denops-plugins.nvim-jdtls.keys"),
+  --event = require("plugins.nvim-jdtls.events"),
+  --event = require("denops-plugins.nvim-jdtls.events"),
+  --dependencies = require("plugins.nvim-jdtls.dependencies"),
+  --dependencies = require("denops-plugins.nvim-jdtls.dependencies"),
+  --init = function()
+  --  -- NOTE: INIT
+  --end,
+  --opts = require("plugins.nvim-jdtls.opts"),
+  --opts = require("denops-plugins.nvim-jdtls.opts"),
+  --config = function()
+  --  local opts = require("plugins.nvim-jdtls.opts")
+  --  local opts = require("denops-plugins.nvim-jdtls.opts")
+  --end,
+  --priority = 1000,
+  cond = false,
+  enabled = false,
+}
+
+return spec
+-- :%s/nvim-jdtls/
