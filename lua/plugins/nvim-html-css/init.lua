dab725198342dc078ca7f252d41cf88cfc9b0c13
Yuto Tanaka <git@mimikun.dev>
2026-07-20 01:42:56 +0900

    wip

--- lua/plugins/nvim-html-css/init.lua
+++ lua/plugins/nvim-html-css/init.lua
@@ -3,13 +3,14 @@ local spec = {
   "Jezda1337/nvim-html-css",
   --lazy = false,
   --ft = require("plugins.nvim-html-css.ft"),
-  --cmd = require("plugins.nvim-html-css.cmds"),
+  cmd = require("plugins.nvim-html-css.cmds"),
   --keys = require("plugins.nvim-html-css.keys"),
   event = require("plugins.nvim-html-css.events"),
   dependencies = require("plugins.nvim-html-css.dependencies"),
   --opts = require("plugins.nvim-html-css.opts"),
   config = function()
     local opts = require("plugins.nvim-html-css.opts")
+    require("html-css").setup(opts)
   end,
   cond = false,
   enabled = false,
