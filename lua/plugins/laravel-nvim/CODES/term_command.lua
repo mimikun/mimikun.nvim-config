e0598837c99f5c49b2317dd7164e9319c9182e78
Yuto Tanaka <git@mimikun.dev>
2026-07-20 01:26:12 +0900

    wip

---
+++ lua/plugins/laravel-nvim/CODES/term_command.lua
@@ -0,0 +1,26 @@
+local BaseCommand = require("laravel.base_command")
+
+local TermCommand = setmetatable({}, { __index = BaseCommand })
+TermCommand.__index = TermCommand
+
+function TermCommand:execute()
+  self.exited = false
+  -- create buffer
+  self.bufnr = vim.api.nvim_create_buf(false, true)
+
+  vim.cmd("botright split")
+  local win = vim.api.nvim_get_current_win()
+  vim.api.nvim_win_set_buf(win, self.bufnr)
+
+  self.job_id = vim.fn.jobstart(self.cmd, {
+    term = true,
+    on_exit = function()
+      self.exited = true
+    end,
+  })
+
+  -- hide window (background)
+  vim.api.nvim_win_hide(win)
+end
+
+return TermCommand
--- lua/plugins/laravel-nvim/init.lua
+++ lua/plugins/laravel-nvim/init.lua
@@ -17,28 +17,19 @@ local spec = {
   --module = false,
   --optional = false,
   --ft = require("plugins.laravel-nvim.ft"),
-  --ft = require("denops-plugins.laravel-nvim.ft"),
   --cmd = require("plugins.laravel-nvim.cmds"),
-  --cmd = require("denops-plugins.laravel-nvim.cmds"),
   --keys = require("plugins.laravel-nvim.keys"),
-  --keys = require("denops-plugins.laravel-nvim.keys"),
   --event = require("plugins.laravel-nvim.events"),
-  --event = require("denops-plugins.laravel-nvim.events"),
   --dependencies = require("plugins.laravel-nvim.dependencies"),
-  --dependencies = require("denops-plugins.laravel-nvim.dependencies"),
   --init = function()
   --  -- NOTE: INIT
   --end,
   --opts = require("plugins.laravel-nvim.opts"),
-  --opts = require("denops-plugins.laravel-nvim.opts"),
   --config = function()
   --  local opts = require("plugins.laravel-nvim.opts")
-  --  local opts = require("denops-plugins.laravel-nvim.opts")
   --end,
-  --priority = 1000,
   cond = false,
   enabled = false,
 }
 
 return spec
--- :%s/laravel-nvim/
