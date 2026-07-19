52f88b71692650e6a3eb43c1d2bf388b9eed1737
Yuto Tanaka <git@mimikun.dev>
2026-07-20 01:43:22 +0900

    wip

---
+++ lua/plugins/nvim-html-css/opts/init.lua
@@ -0,0 +1,46 @@
+---@type table
+local opts = {
+  enable_on = {
+    "html",
+    "htmldjango",
+    "tsx",
+    "jsx",
+    "erb",
+    "svelte",
+    "vue",
+    "blade",
+    "php",
+    "templ",
+    "astro",
+  },
+  handlers = {
+    definition = {
+      bind = "gd",
+    },
+    hover = {
+      bind = "K",
+      wrap = true,
+      border = "none",
+      position = "cursor",
+    },
+  },
+  documentation = {
+    auto_show = true,
+  },
+  peek = {
+    enabled = true,
+    border = "rounded",
+    position = "center", -- "center" | "cursor"
+    width = 0.5, -- fraction of editor width (0.0–1.0)
+    height = 0.5, -- fraction of editor height (0.0–1.0)
+    focus = true, -- whether the float steals focus on open
+    style = "minimal",
+  },
+  style_sheets = {
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
+    "https://cdnjs.cloudflare.com/ajax/libs/bulma/1.0.3/css/bulma.min.css",
+    "./index.css", -- `./` refers to the current working directory.
+  },
+}
+
+return opts
