e5a789d8d7f9d34a34b43e4fb5d0c1078b9c67f5
Yuto Tanaka <git@mimikun.dev>
2026-07-20 14:23:08 +0900

    wip

--- lua/plugins/elixir-tools-nvim/init.lua
+++ lua/plugins/elixir-tools-nvim/init.lua
@@ -17,23 +17,16 @@ local spec = {
   --module = false,
   --optional = false,
   --ft = require("plugins.elixir-tools-nvim.ft"),
-  --ft = require("denops-plugins.elixir-tools-nvim.ft"),
   --cmd = require("plugins.elixir-tools-nvim.cmds"),
-  --cmd = require("denops-plugins.elixir-tools-nvim.cmds"),
   --keys = require("plugins.elixir-tools-nvim.keys"),
-  --keys = require("denops-plugins.elixir-tools-nvim.keys"),
   --event = require("plugins.elixir-tools-nvim.events"),
-  --event = require("denops-plugins.elixir-tools-nvim.events"),
   --dependencies = require("plugins.elixir-tools-nvim.dependencies"),
-  --dependencies = require("denops-plugins.elixir-tools-nvim.dependencies"),
   --init = function()
   --  -- NOTE: INIT
   --end,
   --opts = require("plugins.elixir-tools-nvim.opts"),
-  --opts = require("denops-plugins.elixir-tools-nvim.opts"),
   --config = function()
   --  local opts = require("plugins.elixir-tools-nvim.opts")
-  --  local opts = require("denops-plugins.elixir-tools-nvim.opts")
   --end,
   --priority = 1000,
   cond = false,
@@ -41,4 +34,3 @@ local spec = {
 }
 
 return spec
--- :%s/elixir-tools-nvim/
