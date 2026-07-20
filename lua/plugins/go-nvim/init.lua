2ea8bf041e18534ec3669545b0e010c143c44008
Yuto Tanaka <git@mimikun.dev>
2026-07-20 14:08:23 +0900

    wip

--- lua/plugins/go-nvim/init.lua
+++ lua/plugins/go-nvim/init.lua
@@ -17,23 +17,16 @@ local spec = {
   --module = false,
   --optional = false,
   --ft = require("plugins.go-nvim.ft"),
-  --ft = require("denops-plugins.go-nvim.ft"),
   --cmd = require("plugins.go-nvim.cmds"),
-  --cmd = require("denops-plugins.go-nvim.cmds"),
   --keys = require("plugins.go-nvim.keys"),
-  --keys = require("denops-plugins.go-nvim.keys"),
   --event = require("plugins.go-nvim.events"),
-  --event = require("denops-plugins.go-nvim.events"),
   --dependencies = require("plugins.go-nvim.dependencies"),
-  --dependencies = require("denops-plugins.go-nvim.dependencies"),
   --init = function()
   --  -- NOTE: INIT
   --end,
   --opts = require("plugins.go-nvim.opts"),
-  --opts = require("denops-plugins.go-nvim.opts"),
   --config = function()
   --  local opts = require("plugins.go-nvim.opts")
-  --  local opts = require("denops-plugins.go-nvim.opts")
   --end,
   --priority = 1000,
   cond = false,
@@ -41,4 +34,3 @@ local spec = {
 }
 
 return spec
--- :%s/go-nvim/
