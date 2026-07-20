f9fd065b51ad47d0efde7057bdf86f65d43fa2b2
Yuto Tanaka <git@mimikun.dev>
2026-07-20 17:12:19 +0900

    wip

--- lua/plugins/kotlin-nvim/init.lua
+++ lua/plugins/kotlin-nvim/init.lua
@@ -17,23 +17,16 @@ local spec = {
   --module = false,
   --optional = false,
   --ft = require("plugins.kotlin-nvim.ft"),
-  --ft = require("denops-plugins.kotlin-nvim.ft"),
   --cmd = require("plugins.kotlin-nvim.cmds"),
-  --cmd = require("denops-plugins.kotlin-nvim.cmds"),
   --keys = require("plugins.kotlin-nvim.keys"),
-  --keys = require("denops-plugins.kotlin-nvim.keys"),
   --event = require("plugins.kotlin-nvim.events"),
-  --event = require("denops-plugins.kotlin-nvim.events"),
   --dependencies = require("plugins.kotlin-nvim.dependencies"),
-  --dependencies = require("denops-plugins.kotlin-nvim.dependencies"),
   --init = function()
   --  -- NOTE: INIT
   --end,
   --opts = require("plugins.kotlin-nvim.opts"),
-  --opts = require("denops-plugins.kotlin-nvim.opts"),
   --config = function()
   --  local opts = require("plugins.kotlin-nvim.opts")
-  --  local opts = require("denops-plugins.kotlin-nvim.opts")
   --end,
   --priority = 1000,
   cond = false,
@@ -41,4 +34,3 @@ local spec = {
 }
 
 return spec
--- :%s/kotlin-nvim/
