b77f541b8572e466c177b751b0ea74936e85c9df
Yuto Tanaka <git@mimikun.dev>
2026-07-24 16:16:32 +0900

    wip

--- lua/plugins/searchbox-nvim/init.lua
+++ lua/plugins/searchbox-nvim/init.lua
@@ -2,25 +2,11 @@
 local spec = {
   "VonHeikemen/searchbox.nvim",
   --lazy = false,
-  --url = "",
-  --name = "",
-  --dev = false,
-  --dir = "",
-  --build = "",
-  --branch = "",
-  --tag = "",
-  --version = "",
-  --commit = "",
-  --main = "",
-  --pin = false,
-  --submodules = false,
-  --module = false,
-  --optional = false,
   --ft = require("plugins.searchbox-nvim.ft"),
   --cmd = require("plugins.searchbox-nvim.cmds"),
   --keys = require("plugins.searchbox-nvim.keys"),
-  --event = require("plugins.searchbox-nvim.events"),
-  --dependencies = require("plugins.searchbox-nvim.dependencies"),
+  event = require("plugins.searchbox-nvim.events"),
+  dependencies = require("plugins.searchbox-nvim.dependencies"),
   --init = function()
   --  -- NOTE: INIT
   --end,
@@ -28,7 +14,6 @@ local spec = {
   --config = function()
   --  local opts = require("plugins.searchbox-nvim.opts")
   --end,
-  --priority = 1000,
   cond = false,
   enabled = false,
 }
