ca0b936310a65413024f1153ddb9dd18ca5b0534
Yuto Tanaka <git@mimikun.dev>
2026-07-24 14:55:32 +0900

    wip

---
+++ lua/plugins/searchbox-nvim/init.lua
@@ -0,0 +1,44 @@
+---@type LazySpec
+local spec = {
+  "VonHeikemen/searchbox.nvim",
+  --lazy = false,
+  --url = "",
+  --name = "",
+  --dev = false,
+  --dir = "",
+  --build = "",
+  --branch = "",
+  --tag = "",
+  --version = "",
+  --commit = "",
+  --main = "",
+  --pin = false,
+  --submodules = false,
+  --module = false,
+  --optional = false,
+  --ft = require("plugins.searchbox-nvim.ft"),
+  --ft = require("denops-plugins.searchbox-nvim.ft"),
+  --cmd = require("plugins.searchbox-nvim.cmds"),
+  --cmd = require("denops-plugins.searchbox-nvim.cmds"),
+  --keys = require("plugins.searchbox-nvim.keys"),
+  --keys = require("denops-plugins.searchbox-nvim.keys"),
+  --event = require("plugins.searchbox-nvim.events"),
+  --event = require("denops-plugins.searchbox-nvim.events"),
+  --dependencies = require("plugins.searchbox-nvim.dependencies"),
+  --dependencies = require("denops-plugins.searchbox-nvim.dependencies"),
+  --init = function()
+  --  -- NOTE: INIT
+  --end,
+  --opts = require("plugins.searchbox-nvim.opts"),
+  --opts = require("denops-plugins.searchbox-nvim.opts"),
+  --config = function()
+  --  local opts = require("plugins.searchbox-nvim.opts")
+  --  local opts = require("denops-plugins.searchbox-nvim.opts")
+  --end,
+  --priority = 1000,
+  cond = false,
+  enabled = false,
+}
+
+return spec
+-- :%s/searchbox-nvim/
