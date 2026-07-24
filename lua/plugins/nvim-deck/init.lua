b29ccbf4b7a224ec173a7c4c8501e212b0405735
Yuto Tanaka <git@mimikun.dev>
2026-07-24 14:55:16 +0900

    wip

---
+++ lua/plugins/nvim-deck/init.lua
@@ -0,0 +1,44 @@
+---@type LazySpec
+local spec = {
+  "hrsh7th/nvim-deck",
+  --lazy = false,
+  --url = "",
+  --name = "",
+  --dev = false,
+  --dir = "",
+  --build = "",
+  --branch = "",
+  --tag = "",
+  --version = "",
+  --commit = "",
+  --main = "",
+  --pin = false,
+  --submodules = false,
+  --module = false,
+  --optional = false,
+  --ft = require("plugins.nvim-deck.ft"),
+  --ft = require("denops-plugins.nvim-deck.ft"),
+  --cmd = require("plugins.nvim-deck.cmds"),
+  --cmd = require("denops-plugins.nvim-deck.cmds"),
+  --keys = require("plugins.nvim-deck.keys"),
+  --keys = require("denops-plugins.nvim-deck.keys"),
+  --event = require("plugins.nvim-deck.events"),
+  --event = require("denops-plugins.nvim-deck.events"),
+  --dependencies = require("plugins.nvim-deck.dependencies"),
+  --dependencies = require("denops-plugins.nvim-deck.dependencies"),
+  --init = function()
+  --  -- NOTE: INIT
+  --end,
+  --opts = require("plugins.nvim-deck.opts"),
+  --opts = require("denops-plugins.nvim-deck.opts"),
+  --config = function()
+  --  local opts = require("plugins.nvim-deck.opts")
+  --  local opts = require("denops-plugins.nvim-deck.opts")
+  --end,
+  --priority = 1000,
+  cond = false,
+  enabled = false,
+}
+
+return spec
+-- :%s/nvim-deck/
