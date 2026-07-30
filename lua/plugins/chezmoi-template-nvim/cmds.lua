7ac4840d1ea15c0ec96305607883b4811eca9087
Yuto Tanaka <git@mimikun.dev>
2026-07-30 09:30:12 +0900

    feat: add dpezto/chezmoi-template.nvim

---
+++ lua/plugins/chezmoi-template-nvim/cmds.lua
@@ -0,0 +1,6 @@
+---@type table
+local cmds = {
+  "Chezmoi",
+}
+
+return cmds
---
+++ lua/plugins/chezmoi-template-nvim/dependencies.lua
@@ -0,0 +1,9 @@
+---@type LazySpec[]
+local dependencies = {
+  "stevearc/conform.nvim",
+  "nvim-mini/mini.icons",
+  "andymass/vim-matchup",
+  "saghen/blink.cmp",
+}
+
+return dependencies
