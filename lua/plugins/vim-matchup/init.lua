e84d15fade49eef78a67afb4239ec119abe1de9b
Yuto Tanaka <git@mimikun.dev>
2026-07-30 18:08:57 +0900

    wip

---
+++ lua/plugins/vim-matchup/init.lua
@@ -0,0 +1,21 @@
+---@type LazySpec
+local spec = {
+  "andymass/vim-matchup",
+  --lazy = false,
+  --ft = require("plugins.vim-matchup.ft"),
+  --cmd = require("plugins.vim-matchup.cmds"),
+  --keys = require("plugins.vim-matchup.keys"),
+  --event = require("plugins.vim-matchup.events"),
+  --dependencies = require("plugins.vim-matchup.dependencies"),
+  --init = function()
+  --  -- NOTE: INIT
+  --end,
+  --opts = require("plugins.vim-matchup.opts"),
+  --config = function()
+  --  local opts = require("plugins.vim-matchup.opts")
+  --end,
+  cond = false,
+  enabled = false,
+}
+
+return spec
