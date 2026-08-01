ba9be6a3dccdaa47901839e19496868aeec01945
Yuto Tanaka <git@mimikun.dev>
2026-08-01 13:14:17 +0900

    wip

---
+++ lua/colorschemes/sonokai/init.lua
@@ -0,0 +1,37 @@
+---@type LazySpec
+local spec = {
+  "sainnhe/sonokai",
+  --lazy = false,
+  --url = "",
+  --name = "",
+  --dev = false,
+  --dir = "",
+  --build = "",
+  --branch = "",
+  --tag = "",
+  --version = "",
+  --commit = "",
+  --main = "",
+  --pin = false,
+  --submodules = false,
+  --module = false,
+  --optional = false,
+  --ft = require("colorschemes.sonokai.ft"),
+  --cmd = require("colorschemes.sonokai.cmds"),
+  --keys = require("colorschemes.sonokai.keys"),
+  --event = require("colorschemes.sonokai.events"),
+  --dependencies = require("colorschemes.sonokai.dependencies"),
+  --init = function()
+  --  -- NOTE: INIT
+  --end,
+  --opts = require("colorschemes.sonokai.opts"),
+  --config = function()
+  --  local opts = require("colorschemes.sonokai.opts")
+  --end,
+  --priority = 1000,
+  cond = false,
+  enabled = false,
+}
+
+return spec
+-- :%s/sonokai/
