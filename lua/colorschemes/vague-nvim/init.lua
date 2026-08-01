610723ad31330d8e5b91b017b430014a61310dfa
Yuto Tanaka <git@mimikun.dev>
2026-08-01 12:53:43 +0900

    wip

---
+++ lua/colorschemes/vague-nvim/init.lua
@@ -0,0 +1,37 @@
+---@type LazySpec
+local spec = {
+  "vague-theme/vague.nvim",
+  --lazy = false,
+  --url = "",
+  --name = "",
+  --dev = false,
+  --dir = "",
+  --build = "",
+  --branch = "",
+  --tag = "",
+  --version = "",
+  --commit = "",
+  --main = "",
+  --pin = false,
+  --submodules = false,
+  --module = false,
+  --optional = false,
+  --ft = require("colorschemes.vague-nvim.ft"),
+  --cmd = require("colorschemes.vague-nvim.cmds"),
+  --keys = require("colorschemes.vague-nvim.keys"),
+  --event = require("colorschemes.vague-nvim.events"),
+  --dependencies = require("colorschemes.vague-nvim.dependencies"),
+  --init = function()
+  --  -- NOTE: INIT
+  --end,
+  --opts = require("colorschemes.vague-nvim.opts"),
+  --config = function()
+  --  local opts = require("colorschemes.vague-nvim.opts")
+  --end,
+  --priority = 1000,
+  cond = false,
+  enabled = false,
+}
+
+return spec
+-- :%s/vague-nvim/
