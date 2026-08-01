551fa8073de17824bbbd2998ac03b77f12b995b3
Yuto Tanaka <git@mimikun.dev>
2026-08-01 13:14:15 +0900

    wip

---
+++ lua/colorschemes/dracula-vim/init.lua
@@ -0,0 +1,36 @@
+---@type LazySpec
+local spec = {
+  "dracula/vim",
+  --lazy = false,
+  --url = "",
+  --name = "",
+  --dev = false,
+  --dir = "",
+  --build = "",
+  --branch = "",
+  --tag = "",
+  --version = "",
+  --commit = "",
+  --main = "",
+  --pin = false,
+  --submodules = false,
+  --module = false,
+  --optional = false,
+  --ft = require("colorschemes.dracula-vim.ft"),
+  --cmd = require("colorschemes.dracula-vim.cmds"),
+  --keys = require("colorschemes.dracula-vim.keys"),
+  --event = require("colorschemes.dracula-vim.events"),
+  --dependencies = require("colorschemes.dracula-vim.dependencies"),
+  --init = function()
+  --  -- NOTE: INIT
+  --end,
+  --opts = require("colorschemes.dracula-vim.opts"),
+  --config = function()
+  --  local opts = require("colorschemes.dracula-vim.opts")
+  --end,
+  --priority = 1000,
+  cond = false,
+  enabled = false,
+}
+
+return spec
