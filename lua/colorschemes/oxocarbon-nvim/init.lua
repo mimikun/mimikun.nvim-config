d39f82ab7f83de1d8a26eb495b9805b132ca731e
Yuto Tanaka <git@mimikun.dev>
2026-08-01 12:53:29 +0900

    wip

---
+++ lua/colorschemes/oxocarbon-nvim/init.lua
@@ -0,0 +1,37 @@
+---@type LazySpec
+local spec = {
+  "nyoom-engineering/oxocarbon.nvim",
+  --lazy = false,
+  --url = "",
+  --name = "",
+  --dev = false,
+  --dir = "",
+  --build = "",
+  --branch = "",
+  --tag = "",
+  --version = "",
+  --commit = "",
+  --main = "",
+  --pin = false,
+  --submodules = false,
+  --module = false,
+  --optional = false,
+  --ft = require("colorschemes.oxocarbon-nvim.ft"),
+  --cmd = require("colorschemes.oxocarbon-nvim.cmds"),
+  --keys = require("colorschemes.oxocarbon-nvim.keys"),
+  --event = require("colorschemes.oxocarbon-nvim.events"),
+  --dependencies = require("colorschemes.oxocarbon-nvim.dependencies"),
+  --init = function()
+  --  -- NOTE: INIT
+  --end,
+  --opts = require("colorschemes.oxocarbon-nvim.opts"),
+  --config = function()
+  --  local opts = require("colorschemes.oxocarbon-nvim.opts")
+  --end,
+  --priority = 1000,
+  cond = false,
+  enabled = false,
+}
+
+return spec
+-- :%s/oxocarbon-nvim/
