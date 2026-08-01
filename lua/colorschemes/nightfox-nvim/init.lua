d2190de879527d0e6a9b0a668cbf728202115138
Yuto Tanaka <git@mimikun.dev>
2026-08-01 12:53:18 +0900

    wip

---
+++ lua/colorschemes/nightfox-nvim/init.lua
@@ -0,0 +1,37 @@
+---@type LazySpec
+local spec = {
+  "EdenEast/nightfox.nvim",
+  --lazy = false,
+  --url = "",
+  --name = "",
+  --dev = false,
+  --dir = "",
+  --build = "",
+  --branch = "",
+  --tag = "",
+  --version = "",
+  --commit = "",
+  --main = "",
+  --pin = false,
+  --submodules = false,
+  --module = false,
+  --optional = false,
+  --ft = require("colorschemes.nightfox-nvim.ft"),
+  --cmd = require("colorschemes.nightfox-nvim.cmds"),
+  --keys = require("colorschemes.nightfox-nvim.keys"),
+  --event = require("colorschemes.nightfox-nvim.events"),
+  --dependencies = require("colorschemes.nightfox-nvim.dependencies"),
+  --init = function()
+  --  -- NOTE: INIT
+  --end,
+  --opts = require("colorschemes.nightfox-nvim.opts"),
+  --config = function()
+  --  local opts = require("colorschemes.nightfox-nvim.opts")
+  --end,
+  --priority = 1000,
+  cond = false,
+  enabled = false,
+}
+
+return spec
+-- :%s/nightfox-nvim/
