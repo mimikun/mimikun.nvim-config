b79d10d106847b2a48e9634d1c64dd3d0892515f
Yuto Tanaka <git@mimikun.dev>
2026-08-01 12:53:22 +0900

    wip

---
+++ lua/colorschemes/gruvbox-nvim/init.lua
@@ -0,0 +1,37 @@
+---@type LazySpec
+local spec = {
+  "ellisonleao/gruvbox.nvim",
+  --lazy = false,
+  --url = "",
+  --name = "",
+  --dev = false,
+  --dir = "",
+  --build = "",
+  --branch = "",
+  --tag = "",
+  --version = "",
+  --commit = "",
+  --main = "",
+  --pin = false,
+  --submodules = false,
+  --module = false,
+  --optional = false,
+  --ft = require("colorschemes.gruvbox-nvim.ft"),
+  --cmd = require("colorschemes.gruvbox-nvim.cmds"),
+  --keys = require("colorschemes.gruvbox-nvim.keys"),
+  --event = require("colorschemes.gruvbox-nvim.events"),
+  --dependencies = require("colorschemes.gruvbox-nvim.dependencies"),
+  --init = function()
+  --  -- NOTE: INIT
+  --end,
+  --opts = require("colorschemes.gruvbox-nvim.opts"),
+  --config = function()
+  --  local opts = require("colorschemes.gruvbox-nvim.opts")
+  --end,
+  --priority = 1000,
+  cond = false,
+  enabled = false,
+}
+
+return spec
+-- :%s/gruvbox-nvim/
