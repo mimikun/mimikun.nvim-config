b5f461acb85139e49c5979118341452079f4c4ae
Yuto Tanaka <git@mimikun.dev>
2026-08-10 17:12:33 +0900

    feat: add L3MON4D3/LuaSnip
    Scaffolded with scripts/nvim-plugin-clone.sh. The vendored CODES/ and WIKIS/
    served as the reference for filling in the spec and are not carried into the
    tree, matching the other plugin directories.
    
    The spec has no lazy trigger of its own: blink.cmp pulls LuaSnip in through
    its dependencies and drives expansion and jumping through
    `snippets.preset = "luasnip"`.

---
+++ lua/plugins/LuaSnip/init.lua
@@ -0,0 +1,37 @@
+---@type LazySpec
+local spec = {
+  "L3MON4D3/LuaSnip",
+  --lazy = false,
+  --url = "",
+  --name = "",
+  --dev = false,
+  --dir = "",
+  --build = "",
+  --branch = "",
+  --tag = "",
+  --version = "",
+  --commit = "",
+  --main = "",
+  --pin = false,
+  --submodules = false,
+  --module = false,
+  --optional = false,
+  --ft = require("plugins.LuaSnip.ft"),
+  --cmd = require("plugins.LuaSnip.cmds"),
+  --keys = require("plugins.LuaSnip.keys"),
+  --event = require("plugins.LuaSnip.events"),
+  --dependencies = require("plugins.LuaSnip.dependencies"),
+  --init = function()
+  --  -- NOTE: INIT
+  --end,
+  --opts = require("plugins.LuaSnip.opts"),
+  --config = function()
+  --  local opts = require("plugins.LuaSnip.opts")
+  --end,
+  --priority = 1000,
+  cond = false,
+  enabled = false,
+}
+
+return spec
+-- :%s/LuaSnip/
