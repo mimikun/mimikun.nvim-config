e853d53c7e8b05c6404340cdd74768700c692aa5
Yuto Tanaka <git@mimikun.dev>
2026-08-11 18:53:29 +0900

    wip

---
+++ lua/plugins/buoy-nvim/init.lua
@@ -0,0 +1,37 @@
+---@type LazySpec
+local spec = {
+  "cmfcruz/buoy.nvim",
+  --lazy = false,
+  --url = "",
+  --name = "",
+  --dev = false,
+  --dir = "",
+  --build = "",
+  --branch = "",
+  --tag = "",
+  --version = "",
+  --commit = "",
+  --main = "",
+  --pin = false,
+  --submodules = false,
+  --module = false,
+  --optional = false,
+  --ft = require("plugins.buoy-nvim.ft"),
+  --cmd = require("plugins.buoy-nvim.cmds"),
+  --keys = require("plugins.buoy-nvim.keys"),
+  --event = require("plugins.buoy-nvim.events"),
+  --dependencies = require("plugins.buoy-nvim.dependencies"),
+  --init = function()
+  --  -- NOTE: INIT
+  --end,
+  --opts = require("plugins.buoy-nvim.opts"),
+  --config = function()
+  --  local opts = require("plugins.buoy-nvim.opts")
+  --end,
+  --priority = 1000,
+  cond = false,
+  enabled = false,
+}
+
+return spec
+-- :%s/buoy-nvim/
