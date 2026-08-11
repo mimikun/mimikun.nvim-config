d18d71b5cbd62e4cc2f436c236faf7fafd26615d
Yuto Tanaka <git@mimikun.dev>
2026-08-11 18:53:27 +0900

    wip

---
+++ lua/plugins/kulala-nvim/ft.lua
@@ -0,0 +1,6 @@
+---@type table
+local ft = {
+  "lua",
+}
+
+return ft
---
+++ lua/plugins/kulala-nvim/init.lua
@@ -0,0 +1,37 @@
+---@type LazySpec
+local spec = {
+  "mistweaverco/kulala.nvim",
+  --lazy = false,
+  --url = "",
+  --name = "",
+  --dev = false,
+  --dir = "",
+  --build = "",
+  --branch = "",
+  --tag = "",
+  --version = "",
+  --commit = "",
+  --main = "",
+  --pin = false,
+  --submodules = false,
+  --module = false,
+  --optional = false,
+  --ft = require("plugins.kulala-nvim.ft"),
+  --cmd = require("plugins.kulala-nvim.cmds"),
+  --keys = require("plugins.kulala-nvim.keys"),
+  --event = require("plugins.kulala-nvim.events"),
+  --dependencies = require("plugins.kulala-nvim.dependencies"),
+  --init = function()
+  --  -- NOTE: INIT
+  --end,
+  --opts = require("plugins.kulala-nvim.opts"),
+  --config = function()
+  --  local opts = require("plugins.kulala-nvim.opts")
+  --end,
+  --priority = 1000,
+  cond = false,
+  enabled = false,
+}
+
+return spec
+-- :%s/kulala-nvim/
