13453af8e1271faefb014f7c5a447cbafb7e453c
Yuto Tanaka <git@mimikun.dev>
2026-08-12 11:59:14 +0900

    wip

---
+++ lua/plugins/sidekick-nvim/init.lua
@@ -0,0 +1,37 @@
+---@type LazySpec
+local spec = {
+  "folke/sidekick.nvim",
+  --lazy = false,
+  --url = "",
+  --name = "",
+  --dev = false,
+  --dir = "",
+  --build = "",
+  --branch = "",
+  --tag = "",
+  --version = "",
+  --commit = "",
+  --main = "",
+  --pin = false,
+  --submodules = false,
+  --module = false,
+  --optional = false,
+  --ft = require("plugins.sidekick-nvim.ft"),
+  --cmd = require("plugins.sidekick-nvim.cmds"),
+  --keys = require("plugins.sidekick-nvim.keys"),
+  --event = require("plugins.sidekick-nvim.events"),
+  --dependencies = require("plugins.sidekick-nvim.dependencies"),
+  --init = function()
+  --  -- NOTE: INIT
+  --end,
+  --opts = require("plugins.sidekick-nvim.opts"),
+  --config = function()
+  --  local opts = require("plugins.sidekick-nvim.opts")
+  --end,
+  --priority = 1000,
+  cond = false,
+  enabled = false,
+}
+
+return spec
+-- :%s/sidekick-nvim/
