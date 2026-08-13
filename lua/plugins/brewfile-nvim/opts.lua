a43f552a1b392746ca32ed80bcc64ae1264dd079
Yuto Tanaka <git@mimikun.dev>
2026-08-13 22:48:50 +0900

    feat: add piersolenski/brewfile.nvim

---
+++ lua/plugins/brewfile-nvim/opts.lua
@@ -0,0 +1,10 @@
+---@type table
+local opts = {
+  -- Auto-dump Brewfile after brew commands finish
+  dump_on_change = true,
+
+  -- Show confirmation prompts for uninstall actions
+  confirmation_prompt = true,
+}
+
+return opts
