28bb139d4471ec7f24662067a0b3ee19dcb3552b
Yuto Tanaka <git@mimikun.dev>
2026-08-14 21:56:06 +0900

    feat(lsp): wire SchemaStore catalog into jsonls and yamlls

--- after/lsp/yamlls.lua
+++ after/lsp/yamlls.lua
@@ -1,6 +1,16 @@
--- TODO: yaml ls configs
-
 ---@type vim.lsp.Config
-local config = {}
+local config = {
+  settings = {
+    yaml = {
+      -- NOTE: the built-in schema store must be disabled to use SchemaStore.nvim
+      schemaStore = {
+        enable = false,
+        -- NOTE: empty url avoids a TypeError in yaml-language-server
+        url = "",
+      },
+      schemas = require("schemastore").yaml.schemas(),
+    },
+  },
+}
 
 return config
