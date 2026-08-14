7ee55565eb447a8331f5d0f3d9be4766f90ff8d9
Yuto Tanaka <git@mimikun.dev>
2026-08-14 21:33:05 +0900

    feat: add b0o/SchemaStore.nvim

---
+++ lua/plugins/schemastore-nvim/init.lua
@@ -0,0 +1,10 @@
+---@type LazySpec
+local spec = {
+  "b0o/SchemaStore.nvim",
+  --lazy = false,
+  ft = require("plugins.schemastore-nvim.ft"),
+  --cond = false,
+  --enabled = false,
+}
+
+return spec
