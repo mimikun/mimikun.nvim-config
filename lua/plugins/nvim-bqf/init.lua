6b53954b2d3a962b54eee47aec694c7c1ef0fa14
Yuto Tanaka <git@mimikun.dev>
2026-08-18 22:55:24 +0900

    wip

---
+++ lua/plugins/nvim-bqf/init.lua
@@ -0,0 +1,37 @@
+---@type LazySpec
+local spec = {
+  "kevinhwang91/nvim-bqf",
+  --lazy = false,
+  --url = "",
+  --name = "",
+  --dev = false,
+  --dir = "",
+  --build = "",
+  --branch = "",
+  --tag = "",
+  --version = "",
+  --commit = "",
+  --main = "",
+  --pin = false,
+  --submodules = false,
+  --module = false,
+  --optional = false,
+  --ft = require("plugins.nvim-bqf.ft"),
+  --cmd = require("plugins.nvim-bqf.cmds"),
+  --keys = require("plugins.nvim-bqf.keys"),
+  --event = require("plugins.nvim-bqf.events"),
+  --dependencies = require("plugins.nvim-bqf.dependencies"),
+  --init = function()
+  --  -- NOTE: INIT
+  --end,
+  --opts = require("plugins.nvim-bqf.opts"),
+  --config = function()
+  --  local opts = require("plugins.nvim-bqf.opts")
+  --end,
+  --priority = 1000,
+  cond = false,
+  enabled = false,
+}
+
+return spec
+-- :%s/nvim-bqf/
