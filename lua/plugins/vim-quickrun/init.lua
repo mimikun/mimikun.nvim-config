b648e51870d65f6b9af3ff5af22f5610727f6046
Yuto Tanaka <git@mimikun.dev>
2026-08-25 15:44:39 +0900

    wip

---
+++ lua/plugins/vim-quickrun/init.lua
@@ -0,0 +1,37 @@
+---@type LazySpec
+local spec = {
+  "thinca/vim-quickrun",
+  --lazy = false,
+  --url = "",
+  --name = "",
+  --dev = false,
+  --dir = "",
+  --build = "",
+  --branch = "",
+  --tag = "",
+  --version = "",
+  --commit = "",
+  --main = "",
+  --pin = false,
+  --submodules = false,
+  --module = false,
+  --optional = false,
+  --ft = require("plugins.vim-quickrun.ft"),
+  --cmd = require("plugins.vim-quickrun.cmds"),
+  --keys = require("plugins.vim-quickrun.keys"),
+  --event = require("plugins.vim-quickrun.events"),
+  --dependencies = require("plugins.vim-quickrun.dependencies"),
+  --init = function()
+  --  -- NOTE: INIT
+  --end,
+  --opts = require("plugins.vim-quickrun.opts"),
+  --config = function()
+  --  local opts = require("plugins.vim-quickrun.opts")
+  --end,
+  --priority = 1000,
+  cond = false,
+  enabled = false,
+}
+
+return spec
+-- :%s/vim-quickrun/
