94645641de8564126479e0f4f836fa596bb529b5
Yuto Tanaka <git@mimikun.dev>
2026-09-06 11:31:49 +0900

    wip

--- lua/plugins/markview-nvim/opts/html/void_elements.lua
+++ lua/plugins/markview-nvim/opts/html/void_elements.lua
@@ -8,31 +8,60 @@ local void_elements = {
   -- Configuration for `<string></string>`.
   ---@type markview.config.html.container_elements.opts
   ["^hr$"] = {
-    ---@field on_node table | fun(tag: markview.config.html.void_elements): table Extmark configuration to use on the element.
+    -- Extmark configuration to use on the element.
+    ---@type table | fun(tag: markview.config.html.void_elements): table
     on_node = {
       conceal = "",
-
       virt_text_pos = "inline",
       virt_text = {
-        { "─", "MarkviewGradient2" },
-        { "─", "MarkviewGradient3" },
-        { "─", "MarkviewGradient4" },
-        { "─", "MarkviewGradient5" },
-        { " ◉ ", "MarkviewGradient9" },
-        { "─", "MarkviewGradient5" },
-        { "─", "MarkviewGradient4" },
-        { "─", "MarkviewGradient3" },
-        { "─", "MarkviewGradient2" },
+        {
+          "─",
+          "MarkviewGradient2",
+        },
+        {
+          "─",
+          "MarkviewGradient3",
+        },
+        {
+          "─",
+          "MarkviewGradient4",
+        },
+        {
+          "─",
+          "MarkviewGradient5",
+        },
+        {
+          " ◉ ",
+          "MarkviewGradient9",
+        },
+        {
+          "─",
+          "MarkviewGradient5",
+        },
+        {
+          "─",
+          "MarkviewGradient4",
+        },
+        {
+          "─",
+          "MarkviewGradient3",
+        },
+        {
+          "─",
+          "MarkviewGradient2",
+        },
       },
     },
   },
   ["^br$"] = {
     on_node = {
       conceal = "",
-
       virt_text_pos = "inline",
       virt_text = {
-        { "󱞦", "Comment" },
+        {
+          "󱞦",
+          "Comment",
+        },
       },
     },
   },
